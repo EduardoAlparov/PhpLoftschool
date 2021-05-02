dfdc82dc687b1208278b06a03c92ebdf3d38ffdf
Eduard <workman133@yandex.ru>
2021-05-03 01:52:10 +0300

    HT-2 without burgers 03.05.2021

--- hometask2/hometask2-2/test/src/PaymentInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App;
-
-interface PaymentInterface
-{
-    public function getPriceOfService($minute);
-}
--- hometask2/hometask2-2/test/src/TariffInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface iTariff
-{
-    public function calculatePrice();
-    public function addService(ServiceInterface $service);
-    public function getMinutes(): int;
-    public function getDistance(): int;
-}
